0614265a59e1cf5f7fe960405c0d2ce80b3fc996
Connor Perrone <connorperrone@gmail.com>
2025-10-25 16:57:42 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# RAPID-0-ADCS-Simulink
+
+## Running the Project
+- Clone the repository
+- Open the project (double click the .prj file which runs "initialize.m")
+- Run the model (double click the .slx file in the "models" folder and click "Run")
---
+++ scripts/initialize.m
@@ -0,0 +1,13 @@
+% Initialize variables for Simulink model
+
+% Clear Command Window and Workspace
+clc;
+clear;
+
+% TODO: Decide on variables needed and initialize them here
+% e.g. Moments of Inertia
+% I = [0, 0, 0;
+%      0, 0, 0;
+%      0, 0, 0];
+
+disp("Initialization complete.");
